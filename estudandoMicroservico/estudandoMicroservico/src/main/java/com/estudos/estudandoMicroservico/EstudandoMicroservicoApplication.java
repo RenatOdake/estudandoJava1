c7de3b4f3fdc9f89c992d494cb6aa999c27437d6
RenatOdake <162713886+RenatOdake@users.noreply.github.com>
2024-03-08 13:15:40 -0300

    MAIN:First commit

---
+++ estudandoMicroservico/estudandoMicroservico/src/main/java/com/estudos/estudandoMicroservico/EstudandoMicroservicoApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.estudandoMicroservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudandoMicroservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudandoMicroservicoApplication.class, args);
+	}
+
+}
